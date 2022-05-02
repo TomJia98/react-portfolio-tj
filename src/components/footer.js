2190d73c36586c8194828958392c06e64f496d67
Tom Jia <tomchristian1998@gmail.com>
2022-05-02 16:11:14 +0800

    initial commit

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import github from "../assets/GitHub-Mark-32px.png";
+import linkedIn from "../assets/Li-In-Bug.png";
+
+export default function footer() {
+  return (
+    <footer>
+      <a href="https://github.com/TomJia98">
+        <img src={github} className="logo" alt="github logo"></img>
+      </a>
+      <a href="https://www.linkedin.com/in/tom-jia-222b50226/">
+        <img src={linkedIn} className="logo" alt="linkedIn logo"></img>
+      </a>
+    </footer>
+  );
+}
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+export default function header() {
+  <header>
+    <h1>Tom Jia</h1>
+    <div id="sectionSelection">
+      <h2>About Me</h2>
+      <h2>Portfolio</h2>
+      <h2>Contact</h2>
+      <h2>Resume</h2>
+    </div>
+  </header>;
+}
