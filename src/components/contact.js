67c025b8773c913c654040e7f43d5841f4cbceea
Tom Jia <tomchristian1998@gmail.com>
2022-05-03 12:43:18 +0800

    added a basic contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,23 @@
 export default function contact() {
   return (
     <div>
-      <p>contact section</p>
+      <h3>Contact</h3>
+      <div class="container">
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" placeholder="Your name.." />
+
+        <label htmlFor="email">Email Address</label>
+        <input type="text" id="email" name="email" placeholder="Your email.." />
+
+        <label htmlFor="subject">Subject</label>
+        <textarea
+          id="subject"
+          name="subject"
+          placeholder="Write something.."
+        ></textarea>
+
+        <input type="submit" value="Submit" />
+      </div>
     </div>
   );
 }
